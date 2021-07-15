10e490b9db2d495346e858d461d5b71a525c74de
kwak-bs <iam-bs@naver.com>
2021-07-16 00:46:29 +0900

    FEAT : 헤더 작업 - 반응형 설정 및 대략적인 CSS 목업

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/Navbar.css";
+import { MdSearch, MdNotifications, MdAccountCircle } from "react-icons/md";
 
 const NavBar = () => {
   return (
@@ -10,13 +11,38 @@ const NavBar = () => {
             <h1 className="navbar_name">wanted</h1>
           </div>
           <ul className="navbar_ul">
-            <li className="navbar_li">탐색</li>
+            <li className="navbar_li selected">탐색</li>
             <li className="navbar_li">커리어 성장</li>
             <li className="navbar_li">직군별 연봉</li>
             <li className="navbar_li">이력서</li>
             <li className="navbar_li">매치업</li>
             <li className="navbar_li">프리랜서</li>
           </ul>
+          
+          <aside className="navbar_aside">
+            <ul className="aside_ul">
+              <li className="aside_li">
+                <button type="button" className="searchButton">
+                  <MdSearch size="20" color="black" />
+                </button>
+              </li>
+              <li className="aside_li">
+                <button type="button" className="notiButton">
+                  <MdNotifications size="20" color="black" />
+                </button>
+              </li>
+              <li className="aside_li">
+                <button type="button" className="accButton">
+                  <MdAccountCircle size="20" color="black" />
+                </button>
+              </li>
+              <li className="aside_li">
+                <button type="button" className="svcButton">
+                  기업 서비스
+                </button>
+              </li>
+            </ul>
+          </aside>
         </nav>
       </div>
     </div>
