4f33da9ee733d16199aae9e534b97f514947c2dd
kwak-bs <iam-bs@naver.com>
2021-07-16 15:56:52 +0900

    CSS : 배열과 map을 이용해서 tab list 재구성 active underline을 간결하기 나타내기 위함 기존에는 classList에 toggle로 해결하려했지만 서로 다른 탭을 누를 때 기존 탭 underline이 없어지지 않았음

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/Navbar.css";
 import { MdSearch, MdNotifications, MdAccountCircle } from "react-icons/md";
 
 const NavBar = () => {
+  const tabClickHandler = (index) => {
+    setSelectedIndex(index);
+    console.log(index + " 클릭됨");
+  };
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const tabArray = [
+    {
+      tabTitle: (
+        <li
+          className={selectedIndex === 0 ? "navbar_li selected" : "navbar_li"}
+          onClick={() => tabClickHandler(0)}
+        >
+          탐색
+        </li>
+      ),
+    },
+    {
+      tabTitle: (
+        <li
+          className={selectedIndex === 1 ? "navbar_li selected" : "navbar_li"}
+          onClick={() => tabClickHandler(1)}
+        >
+          커리어 성장
+        </li>
+      ),
+    },
+    {
+      tabTitle: (
+        <li
+          className={selectedIndex === 2 ? "navbar_li selected" : "navbar_li"}
+          onClick={() => tabClickHandler(2)}
+        >
+          직군별 연봉
+        </li>
+      ),
+    },
+    {
+      tabTitle: (
+        <li
+          className={selectedIndex === 3 ? "navbar_li selected" : "navbar_li"}
+          onClick={() => tabClickHandler(3)}
+        >
+          이력서
+        </li>
+      ),
+    },
+    {
+      tabTitle: (
+        <li
+          className={selectedIndex === 4 ? "navbar_li selected" : "navbar_li"}
+          onClick={() => tabClickHandler(4)}
+        >
+          매치업
+        </li>
+      ),
+    },
+    {
+      tabTitle: (
+        <li
+          className={selectedIndex === 5 ? "navbar_li selected" : "navbar_li"}
+          onClick={() => tabClickHandler(5)}
+        >
+          프리랜서
+        </li>
+      ),
+    },
+  ];
   return (
     <div role="navigation" className="nav_container">
       <div className="nav_container_flex">
@@ -11,14 +79,11 @@ const NavBar = () => {
             <h1 className="navbar_name">wanted</h1>
           </div>
           <ul className="navbar_ul">
-            <li className="navbar_li selected">탐색</li>
-            <li className="navbar_li">커리어 성장</li>
-            <li className="navbar_li">직군별 연봉</li>
-            <li className="navbar_li">이력서</li>
-            <li className="navbar_li">매치업</li>
-            <li className="navbar_li">프리랜서</li>
+            {tabArray.map((section, index) => {
+              return section.tabTitle;
+            })}
           </ul>
-          
+
           <aside className="navbar_aside">
             <ul className="aside_ul">
               <li className="aside_li">
